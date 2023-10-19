c8214af6128dd39cfb8e9660f3cbd63df8a1752f
shaunsargent <ShaunM10@gmail.com>
2023-10-19 06:13:53 -0400

    adding DTO's

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,17 @@
+using System.Runtime;
+
+namespace AuctionService.Entities;
+
+public class Auction
+{
+    public Guid Id { get; set; }
+    public decimal ReservedPrice { get; set; } = 0;
+    public string? Seller { get; set; } = null;
+    public string? Winner { get; set; } = null;
+    public decimal SoldAmount { get; set; }
+    public decimal CurrentHighBid { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public Status Status { get; set; }
+    public Item Item { get; set; }
+}
