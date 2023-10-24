444f7fac7ae3d3b99698b38e5ca25572e068c8e1
shaunsargent <ShaunM10@gmail.com>
2023-10-24 11:18:15 -0400

    Updated AuctionController

--- src/AuctionService/Controllers/AuctionController.cs
+++ src/AuctionService/Controllers/AuctionController.cs
@@ -2,6 +2,7 @@ using AuctionService.Data;
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,17 +22,29 @@ public class AuctionController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<List<AuctionDto>> GetAllAuctions()
+    public async Task<List<AuctionDto>> GetAllAuctions(string? date)
     {
-        var auctions = await this.context
-            .Auctions
-            .Include(x => x.Item)
+        var query = this.context.Auctions
             .OrderBy(x => x.Item.Make)
-            .ThenBy(x => x.Item.Model)
+            .AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(date))
+        {
+            query = query
+                .Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+        }
+        return await query.ProjectTo<AuctionDto>(this.mapper.ConfigurationProvider)
             .ToListAsync();
 
-        var response = this.mapper.Map<List<AuctionDto>>(auctions);
-        return response;
+        // var auctions = await this.context
+        //     .Auctions
+        //     .Include(x => x.Item)
+        //     .OrderBy(x => x.Item.Make)
+        //     .ThenBy(x => x.Item.Model)
+        //     .ToListAsync();
+
+        // var response = this.mapper.Map<List<AuctionDto>>(auctions);
+        // return response;
     }
 
     [HttpGet("{id}")]
