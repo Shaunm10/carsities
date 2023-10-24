c02c537eb9de2d15f5246a9c4c3ff09182071234
shaunsargent <ShaunM10@gmail.com>
2023-10-24 09:47:54 -0400

    Added pageSize and pageNumber

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -10,9 +10,9 @@ namespace SearchService.Controllers
     {
 
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+        public async Task<ActionResult<List<Item>>> SearchItems(string? searchTerm, int pageNumber = 1, int pageSize = 4)
         {
-            var query = DB.Find<Item>();
+            var query = DB.PagedSearch<Item>();
 
             query.Sort(x => x.Ascending(a => a.Make));
 
@@ -23,8 +23,16 @@ namespace SearchService.Controllers
                     .SortByTextScore();
             }
 
+            query.PageNumber(pageNumber);
+            query.PageSize(pageSize);
+
             var result = await query.ExecuteAsync();
-            return result;
+            return this.Ok(new
+            {
+                results = result.Results,
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount
+            });
         }
     }
 }
