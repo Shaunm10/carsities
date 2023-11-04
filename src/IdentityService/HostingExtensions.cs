dac404e7f68a627e1aa24ef40500a3ca4c5bf4c4
shaunsargent <ShaunM10@gmail.com>
2023-11-04 19:08:45 -0400

    upgrading packages

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -35,26 +35,26 @@ internal static class HostingExtensions
             .AddInMemoryApiScopes(Config.ApiScopes)
             .AddInMemoryClients(Config.Clients)
             .AddAspNetIdentity<ApplicationUser>();
-        
-        builder.Services.AddAuthentication()
-            .AddGoogle(options =>
-            {
-                options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
 
-                // register your IdentityServer with Google at https://console.developers.google.com
-                // enable the Google+ API
-                // set the redirect URI to https://localhost:5001/signin-google
-                options.ClientId = "copy client ID from Google here";
-                options.ClientSecret = "copy client secret from Google here";
-            });
+        builder.Services.AddAuthentication();
+        // .AddGoogle(options =>
+        // {
+        //     options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+        //     // register your IdentityServer with Google at https://console.developers.google.com
+        //     // enable the Google+ API
+        //     // set the redirect URI to https://localhost:5001/signin-google
+        //     options.ClientId = "copy client ID from Google here";
+        //     options.ClientSecret = "copy client secret from Google here";
+        // });
 
         return builder.Build();
     }
-    
+
     public static WebApplication ConfigurePipeline(this WebApplication app)
-    { 
+    {
         app.UseSerilogRequestLogging();
-    
+
         if (app.Environment.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
@@ -64,7 +64,7 @@ internal static class HostingExtensions
         app.UseRouting();
         app.UseIdentityServer();
         app.UseAuthorization();
-        
+
         app.MapRazorPages()
             .RequireAuthorization();
 
