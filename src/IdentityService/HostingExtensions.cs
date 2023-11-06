c9551425fc7589f221c599578463623043636212
Shaunm10 <shaunm10@gmail.com>
2023-11-06 16:20:50 -0500

    fixed configuration, seed data and migraiton script for the IDP

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -37,6 +37,8 @@ internal static class HostingExtensions
             .AddInMemoryClients(Config.Clients)
             .AddAspNetIdentity<ApplicationUser>();
 
+        builder.Services.ConfigureApplicationCookie(o => o.Cookie.SameSite = SameSiteMode.Lax);
+
         builder.Services.AddAuthentication();
 
         return builder.Build();
