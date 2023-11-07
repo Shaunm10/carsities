46b8aaad3672092b42bad29c958b496b58427942
Shaunm10 <shaunm10@gmail.com>
2023-11-07 16:47:15 -0500

    Added registration page code behind + Model

--- src/IdentityService/Pages/Account/Register/Index.cshtml.cs
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -1,4 +1,10 @@
+using System.Security.Claims;
+using IdentityModel;
+using IdentityService.Models;
+using IdentityService.Pages.Account.Register;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IdentityService.Pages.Register;
@@ -7,8 +13,58 @@ namespace IdentityService.Pages.Register;
 [AllowAnonymous]
 public class Index : PageModel
 {
-    public void OnGet()
-    {
+    private readonly UserManager<ApplicationUser> userManager;
 
+    [BindProperty]
+    public RegisterViewModel Input { get; set; }
+
+    [BindProperty]
+    public bool RegistrationSuccess { get; set; }
+
+    public Index(UserManager<ApplicationUser> userManager)
+    {
+        this.userManager = userManager;
+    }
+
+    public IActionResult OnGet(string returnUrl)
+    {
+        Input = new RegisterViewModel
+        {
+            ReturnUrl = returnUrl
+        };
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (this.Input.Button != "register")
+        {
+            return this.Redirect("~/");
+        }
+
+        // verify things are valid,
+        if (this.ModelState.IsValid)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = this.Input.Username,
+                Email = this.Input.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await this.userManager.CreateAsync(user, this.Input.Password);
+
+            if (result.Succeeded)
+            {
+                await this.userManager.AddClaimsAsync(user, new Claim[]{
+                     new Claim(JwtClaimTypes.Name, this.Input.FullName)
+                });
+
+                this.RegistrationSuccess = true;
+            }
+        }
+
+        return this.Page();
     }
 }
