55c11abd7995059ba45689699f01cd08ba463f6e
shaun sargent <shaunm10@gmail.com>
2023-11-10 16:36:23 -0500

    Added ReverseProxy to middleware

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,8 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+app.MapReverseProxy();
 
 app.MapGet("/", () => "Hello World!");
 
