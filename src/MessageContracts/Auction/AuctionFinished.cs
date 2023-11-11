31e1297e5c7e7a642a6f3c9499d5b8a92706519b
shaun sargent <shaunm10@gmail.com>
2023-11-11 08:37:42 -0500

    Added AuctionFinished record

---
+++ src/MessageContracts/Auction/AuctionFinished.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace MessageContracts.Auction;
+
+public record AuctionFinished
+{
+    public bool ItemSold { get; set; }
+    public string AuctionId { get; set; }
+    public string Winner { get; set;}
+    public string Seller { get; set; }
+    public decimal Amount { get; set; }
+}
