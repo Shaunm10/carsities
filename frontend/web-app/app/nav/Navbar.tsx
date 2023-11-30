a3db1b643e7c9c15d2bbd08180672855af1c210e
shaunsargent <ShaunM10@gmail.com>
2023-11-30 18:27:47 -0500

    made the nav items camelCase

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -2,12 +2,12 @@
 import React from 'react';
 
 export default function Navbar() {
-  console.log('client component');
-  return (
-    <header className="sticky top-0 z-50 flex justify-between">
-      <div>Left</div>
-      <div>Middle</div>
-      <div>Right</div>
-    </header>
-  );
+	console.log('client component');
+	return (
+		<header className="sticky top-0 z-50 flex justify-between">
+			<div>left</div>
+			<div>middle</div>
+			<div>right</div>
+		</header>
+	);
 }
