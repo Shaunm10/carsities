a9b4dbe935840ae529b8ab2727f02fd496e648ce
Shaunm10 <shaunm10@gmail.com>
2023-12-08 16:46:48 -0500

    Added CarImage BlurEffect

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import Image from 'next/image';
 import CountdownTimer from './CountdownTimer';
+import { CarImage } from './CarImage';
 
 type Props = { auction: any };
 export const AuctionCard = ({ auction }: Props) => {
   return (
-    <a href="#">
+    <a href="#" className="group">
       <div className="w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden">
         <div>
-          <Image
-            src={auction.imageUrl}
-            alt="image"
-            fill
-            priority
-            className="object-cover"
-            sizes="(max-width:768px) 100vw, (max-width: 1200px) 50vw, 25vw"
-          ></Image>
+          <CarImage imageUrl={auction.imageUrl} />
+          <div className="absolute bottom-2 left-2">
+            <CountdownTimer auctionEnd={auction.auctionEnd} />
+          </div>
         </div>
       </div>
       <div className="flex justify-between items-center mt-4">
@@ -24,7 +21,6 @@ export const AuctionCard = ({ auction }: Props) => {
         </h3>
         <p className="font-semibold text-sm">{auction.year}</p>
       </div>
-      <CountdownTimer auctionEnd={auction.auctionEnd} />
     </a>
   );
 };
--- frontend/web-app/app/auctions/CountdownTimer.tsx
+++ frontend/web-app/app/auctions/CountdownTimer.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import Countdown from 'react-countdown';
+import Countdown, { zeroPad } from 'react-countdown';
 
 type Props = { auctionEnd: string };
 const renderer = ({
@@ -16,17 +16,33 @@ const renderer = ({
   seconds: number;
   completed: boolean;
 }) => {
-  if (completed) {
-    // render a completed state
-    return <span>Finished</span>;
-  } else {
-    // render a countdown
-    return (
-      <span>
-        {days}:{hours}:{minutes}:{seconds}
-      </span>
-    );
-  }
+  return (
+    <div
+      className={` border-2
+	   border-white
+	    text-white
+		 py-1 px-2 
+		 rounded-lg 
+		 flex 
+		 justify-center
+	 	${
+      completed
+        ? 'bg-red-600'
+        : days === 0 && hours < 10
+        ? 'bg-amber-600'
+        : 'bg-green-600'
+    } 
+	  `}
+    >
+      {completed ? (
+        <span>Auction finished</span>
+      ) : (
+        <span suppressHydrationWarning={true}>
+          {zeroPad(days)}:{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
+        </span>
+      )}
+    </div>
+  );
 };
 const CountdownTimer = ({ auctionEnd }: Props) => {
   return (
