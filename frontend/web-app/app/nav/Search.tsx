7d33d7ada326436cc2538037c20afa285f6d0bf7
Shaunm10 <shaunm10@gmail.com>
2023-12-18 17:01:16 -0500

    - working on header search

--- frontend/web-app/app/nav/Search.tsx
+++ frontend/web-app/app/nav/Search.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+'use client';
+import { useParamsStore } from '@/hooks/useParamsStore';
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
+import { setEnvironmentData } from 'worker_threads';
 
 export const Search = () => {
+  const setParams = useParamsStore((state) => state.setParams);
+  const [searchInput, setSearchInput] = useState('');
+
+  function searchOnChange(event: ChangeEvent<HTMLInputElement>) {
+    setSearchInput(event.target.value);
+  }
+
+  function search() {
+    setParams({ searchTerm: searchInput });
+  }
   return (
     <div className="flex w-[50%] items-center border-2 rounded-full py-2 shadow-sm">
       <input
+        onKeyDown={(e: KeyboardEvent) => {
+          if (e.key === 'Enter') {
+            search();
+          }
+        }}
+        onChange={searchOnChange}
         type="text"
         placeholder="Search for cars by make, model or color"
         className="
@@ -19,7 +38,7 @@ export const Search = () => {
         text-gray-600
         "
       />
-      <button>
+      <button onClick={search}>
         <FaSearch
           size={34}
           className="bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2"
