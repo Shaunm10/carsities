f175c413dff757476a8c32a2f8a90e10808ca2e3
shaun sargent <shaunm10@gmail.com>
2023-12-27 12:59:14 -0500

    Added a NoResults view to finishe up the search functionality

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -9,6 +9,8 @@ import { Filters } from './Filters';
 import { shallow } from 'zustand/shallow';
 import qs from 'query-string';
 import { useParamsStore } from '@/hooks/useParamsStore';
+import { Heading } from '../components/Heading';
+import { EmptyFilter } from '../components/EmptyFilter';
 
 export const Listings = () => {
   const [data, setData] = useState<PagedResults<Auction>>();
@@ -50,20 +52,25 @@ export const Listings = () => {
   return (
     <>
       <Filters />
-      <div className='grid grid-cols-4 gap-6'>
-        {data.results &&
-          data.results.map((auction) => (
-            <AuctionCard auction={auction} key={auction.id} />
-          ))}
-      </div>
-      <div className='flex justify-center mt-4'>
-        <AppPagination
-          pageChanged={setPageNumber}
-          currentPage={params.pageNumber}
-          pageCount={data.pageCount}
-        />
-      </div>
-      <div></div>
+      {data.totalCount === 0 ? (
+        <EmptyFilter showReset />
+      ) : (
+        <>
+          <div className='grid grid-cols-4 gap-6'>
+            {data.results &&
+              data.results.map((auction) => (
+                <AuctionCard auction={auction} key={auction.id} />
+              ))}
+          </div>
+          <div className='flex justify-center mt-4'>
+            <AppPagination
+              pageChanged={setPageNumber}
+              currentPage={params.pageNumber}
+              pageCount={data.pageCount}
+            />
+          </div>
+        </>
+      )}
     </>
   );
 };
