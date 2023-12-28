9db7b52b22c2882b6fe3eca1066c48cc48e394f3
Shaunm10 <shaunm10@gmail.com>
2023-12-28 12:16:58 -0500

    added idToken property

--- frontend/web-app/app/api/auth/[...nextauth]/route.ts
+++ frontend/web-app/app/api/auth/[...nextauth]/route.ts
@@ -16,6 +16,7 @@ export const authOptions: NextAuthOptions = {
           scope: 'openid profile auctionApp',
         },
       },
+      idToken: true, // this will ask the IDP for the userId
     }),
   ],
 };
