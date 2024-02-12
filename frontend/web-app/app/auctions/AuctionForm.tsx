b859cd9b137fbd29063c9b0137c8ca6e62ce9879
shaunsargent <ShaunM10@gmail.com>
2024-02-12 14:50:38 -0500

    Added Input component

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -2,12 +2,14 @@
 import { Button, TextInput } from 'flowbite-react';
 import React from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
+import Input from '../components/input';
 
 export const AuctionForm = () => {
   'use client';
   const {
     register,
     handleSubmit,
+    control,
     setFocus,
     formState: { isDirty, isSubmitting, isValid, errors },
   } = useForm();
@@ -17,7 +19,19 @@ export const AuctionForm = () => {
 
   return (
     <form className='flex flex-col mt-3' onSubmit={handleSubmit(onSubmit)}>
-      <div className='mb-3 block'>
+      <Input
+        label='Make'
+        name='make'
+        control={control}
+        rules={{ required: 'Make is required' }}
+      />
+      <Input
+        label='Model'
+        name='model'
+        control={control}
+        rules={{ required: 'Model is required' }}
+      />
+      {/* <div className='mb-3 block'>
         <TextInput
           {...register('make', { required: 'Make is required' })}
           placeholder='Make'
@@ -32,7 +46,7 @@ export const AuctionForm = () => {
           color={errors?.model && 'failure'}
           helperText={errors.model?.message as string}
         />
-      </div>
+      </div> */}
       <div className='flex justify-between'>
         <Button outline color='grey'>
           Cancel
--- frontend/web-app/app/components/input.tsx
+++ frontend/web-app/app/components/input.tsx
@@ -1,13 +1,38 @@
+import { Label, TextInput } from 'flowbite-react';
 import React from 'react';
 import { UseControllerProps, useController } from 'react-hook-form';
 
 type Props = {
   label: string;
   type?: string;
-  showLabel: boolean;
+  showLabel?: boolean;
 } & UseControllerProps;
 
-export const input = (props: Props) => {
+const Input = (props: Props) => {
   const { fieldState, field } = useController({ ...props, defaultValue: '' });
-  return <div className='mb-3'>input</div>;
+  const color = fieldState.error
+    ? 'failure'
+    : !fieldState.isDirty
+    ? ''
+    : 'success';
+  return (
+    <div className='mb-3'>
+      {props.showLabel && (
+        <div className='mb-2 block'>
+          <Label htmlFor={field.name} value={props.label}></Label>
+        </div>
+      )}
+
+      <TextInput
+        {...props}
+        {...field}
+        placeholder={props.label}
+        type={props.type || 'text'}
+        color={color}
+        helperText={fieldState.error?.message}
+      />
+    </div>
+  );
 };
+
+export default Input;
