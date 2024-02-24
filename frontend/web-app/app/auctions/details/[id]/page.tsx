d1b95a775c30dc6b585641e251a0bb6b240807fa
shaun sargent <shaunm10@gmail.com>
2024-02-24 14:55:19 -0500

    - WIP

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -1,9 +1,20 @@
+'use server';
+import { getDetailedViewData } from '@/app/actions/auctionActions';
+import { Heading } from '@/app/components/Heading';
 import React from 'react';
 
 type props = { params: { id: string } };
 
-const Details = ({ params }: props) => {
-  return <div>Details for {params.id}</div>;
+// NOTE: you should only use async on the component if it's
+// a server component.
+const Details = async ({ params }: props) => {
+  const data = await getDetailedViewData(params.id);
+
+  return (
+    <div>
+      <Heading title={`${data.make} ${data.model}`} subtitle='' />
+    </div>
+  );
 };
 
 export default Details;
