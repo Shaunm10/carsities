575338b5bebc5b69d8bbf716348edddf41d63e0b
shaunsargent <ShaunM10@gmail.com>
2024-03-03 17:06:24 -0500

    - added more configuration for BiddingService.

--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -1,3 +1,5 @@
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MongoDB.Driver;
 using MongoDB.Entities;
 
@@ -6,6 +8,38 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("bids", false));
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMQ:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMQ:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMQ:Password", "guest"));
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+
+        // TLS is handled at the Router/K(8)s, not here.
+        options.RequireHttpsMetadata = false;
+
+        options.TokenValidationParameters.ValidateAudience = false;
+
+        // this allows us to use User.Identity.Name in the Controller to
+        // get the "username" claim value.
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
@@ -24,6 +58,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 // initialises a new Mongo database.
+Console.WriteLine($"MongoDB connection is: {builder.Configuration.GetConnectionString("BidDbConnection")}");
 await DB.InitAsync("BidDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("BidDbConnection")));
 
 app.Run();
