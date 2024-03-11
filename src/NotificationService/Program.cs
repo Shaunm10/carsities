e0dfe1a7429c5e718745eb0047c2931c8668d6cf
shaunsargent <ShaunM10@gmail.com>
2024-03-10 21:25:07 -0400

    - fixed the docker compose build.

---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,25 @@
+using MassTransit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+    //x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMQ:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMQ:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMQ:Password", "guest"));
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+var app = builder.Build();
+
+app.Run();
