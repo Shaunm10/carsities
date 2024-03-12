02efd81b89e9aadf5dbbd5a744947aaaeb1dfe06
Shaunm10 <shaunm10@gmail.com>
2024-03-12 12:59:12 -0400

    Added Configuration for Notificiation services in Proxy + Hub

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -1,4 +1,5 @@
 using MassTransit;
+using NotificationService.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,10 @@ builder.Services.AddMassTransit(x =>
     });
 });
 
+builder.Services.AddSignalR();
+
 var app = builder.Build();
+
+app.MapHub<NotificationHub>("/notifications");
 
 app.Run();
